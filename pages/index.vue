f7e8564b8da9d407fac2b9dbdc9f09e78d444279
vuchl <vogel_sebastian1@freenet.de>
2017-09-26 07:56:01 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello from Nuxt! This is {{which}}-side rendered.</h1>
+</template>
+</<script>
+export default {
+  asyncData ({ isStatic, isServer }) {
+    return {
+      which: isStatic ? 'static' : (isServer ? 'server' : 'client')
+    }
+  }
+}
+</script>
